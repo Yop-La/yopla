d2464ab625af06b3822d54e354a002fcd9a4093b
Alexandre Guillemine <alex.guillemine@gmail.com>
2018-12-21 12:47:25 +0100

    hello world

--- search_engine/views.py
+++ search_engine/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+
+    """ Exemple de page non valide au niveau HTML pour que l'exemple soit concis """
+
+    return HttpResponse("""
+
+        <h1>Bienvenue sur mon blog !</h1>
+
+        <p>Les crêpes bretonnes ça tue des mouettes en plein vol !</p>
+
+    """)
--- yopla/urls.py
+++ yopla/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from search_engine import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home),
+
 ]
